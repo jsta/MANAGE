4149ab28f901ee5fbc63aa212329eba8fc8ff126
jsta <stachel2@msu.edu>
2018-03-05 13:54:19 -0500

    save compiled data object

--- getMANAGE.R
+++ getMANAGE.R
@@ -2,3 +2,4 @@ download.file("https://www.ars.usda.gov/ARSUserFiles/30980500/manage2016.zip", "
 unzip("MANAGE.zip")
 
 dt <- Hmisc::mdb.get("managev4ag+forest+yield+drain4-1-16.accdb")
+saveRDS(dt, "manage.rds")
